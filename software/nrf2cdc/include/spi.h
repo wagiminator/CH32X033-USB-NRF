fef35a9d4a28aa700be363455e94553cb4e4d213
wagiminator <wagiminator@web.de>
2024-03-09 17:22:45 +0100

    Initial commit

--- software/nrf2cdc/include/spi.h
+++ software/nrf2cdc/include/spi.h
@@ -36,7 +36,7 @@ extern "C" {
 #include "system.h"
 
 // SPI Parameters
-#define SPI_PRESC           6     // SPI_CLKRATE = F_CPU / (2 << SPI_PRESC)
+#define SPI_PRESC           2     // SPI_CLKRATE = F_CPU / (2 << SPI_PRESC)
 
 // I2C Functions and Macros
 #define SPI_busy()          (SPI1->STATR & SPI_STATR_BSY)
